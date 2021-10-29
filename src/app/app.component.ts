c31c77b57c7e9136b86cebec8d35aac72f4bdbf4
vgo <vgo@ccabe.com>
2021-10-29 17:00:46 +0200

    Intégrez Firebase à votre application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'library';
+  
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyDVYG0a2QbI3iYAFiGUSrr3j2FfNNXjFJQ",
+      authDomain: "library-b284c.firebaseapp.com",
+      projectId: "library-b284c",
+      storageBucket: "library-b284c.appspot.com",
+      messagingSenderId: "283455867462",
+      appId: "1:283455867462:web:738fb29df8a192f1f9d847"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, resolveForwardRef } from '@angular/core';
+import { rejects } from 'assert';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,39 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string){
+    return new Promise(
+      (resolve, rejects) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve
+          },
+          (error) => {
+            rejects(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email: string, password: string){
+    return new Promise(
+      (resolve, rejects) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve;
+          },
+          (error) => {
+            rejects(error);
+          }
+        )
+      }
+    );
+  }
+
+  signOutUser() {
+    firebase.auth().signOut();
+  }
+
 }
